a1f1577e0cab479ba2d2fb26deb56a1dc6a0a636
Student-1-web-star <miss.anan.students@gmail.com>
2024-07-12 18:23:05 +0300

    Add files via upload

---
+++ final.js
@@ -0,0 +1,13 @@
+var btnSubmit = document.querySelector(".submit");
+var modal = document.querySelector(".modal-container");
+
+if (btnSubmit) {
+    btnSubmit.addEventListener("click", function () {
+        modal.classList.add("show");
+        
+        // const next = document.querySelector(".next");
+        // next.setAttribute("value", "http://127.0.0.1:5500/contact.html");
+    })
+}
+
+document.querySelectorAll(".")
